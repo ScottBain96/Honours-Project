f5b9d75fe200f03b29892c7d5a3369361b712d8a
Scott PC UK <40214330@live.napier.ac.uk>
2019-01-24 01:45:29 +0000

    Final Folder Fix
    Fixed all the files and names//WORKING

---
+++ xamarinFitnessApp/xamarinFitnessApp/MainMenu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarinFitnessApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainMenu : ContentPage
+	{
+		public MainMenu ()
+		{
+			InitializeComponent ();
+		}
+
+
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            //myLabel.Text = "Changed";
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void ImageButton_Clicked_Tools(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ToolsPage());
+        }
+
+
+    }
+}
