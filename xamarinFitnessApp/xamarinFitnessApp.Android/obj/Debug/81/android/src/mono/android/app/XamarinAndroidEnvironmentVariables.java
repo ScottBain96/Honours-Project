4e74a4a9339aa54103a732a0dc43c618d5695db2
Scott PC UK <40214330@live.napier.ac.uk>
2019-05-22 06:28:47 +0000

    backup

--- xamarinFitnessApp/xamarinFitnessApp.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ xamarinFitnessApp/xamarinFitnessApp.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "877be2e2-b838-41e8-8f86-c4eb1c63a97e",
+		"XAMARIN_BUILD_ID", "091b23ec-dc4e-4c3e-bf24-a126c37f390c",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep",
